b3e67567cb2fe4a6d4bf5f661d71a427c3ec124d
cameron <cameron.murtagh00@gmail.com>
2022-03-05 13:52:14 +1100

    remove neovim from dots

--- .config/nvim/docker_start.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker container run -i --rm --mount 'type=volume,source=nubots_generic_build,target=/home/nubots/build,consistency=delegated' --mount 'type=bind,source=/home/cameron/Documents/NUbots,target=/home/nubots/NUbots,consistency=cached' --mount 'type=bind,source=/home/cameron/Documents/NUbots,target=/home/cameron/Documents/NUbots,readonly' --workdir /home/nubots/NUbots nubots:selected clangd --compile-commands-dir=/home/nubots/build --background-index --clang-tidy 2>/home/cameron/log.txt
